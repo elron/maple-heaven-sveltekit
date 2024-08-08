f18cb2931fac7afd098cd65f9bd8b51ba0f3b539
Elron <elron100duck@gmail.com>
2024-08-08 22:16:44 +0300

    tackles 16-issue 15-issue and 17-issue

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,37 @@
+<div class="content">
+  <h1>About</h1>
+
+  <p>
+    Hi, We're two brothers who play MapleLegends because we wanted to feel the
+    nostalgic experience again.
+  </p>
+
+  <p>
+    We wanted to open stores in FM to earn some mesos, but didn't know how to
+    price our items. We created <a href="/">Maple Heaven</a> to help us easily find items data
+    and market prices.
+  </p>
+
+  <p>
+    Maple Heaven project is developed using <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a>, and the data presented on
+    our website is based on <a href="https://owlrepo.com/" target="_blank"
+      >OwlRepo.com</a
+    >
+    with a friendly UI combining item icons and item data from
+    <a href="https://bbb.hidden-street.net/" target="_blank"
+      >Hidden-Street.net (before big bang)</a
+    >.
+  </p>
+
+  <p>
+    Feel free to <a
+      href="https://github.com/elron/maple-heaven-sveltekit"
+      target="_blank">Contribute on GitHub</a
+    >.
+  </p>
+
+  <p>
+    The Maple Brothers<br />
+    E and O
+  </p>
+</div>
