f3f9ce0069622881f467ebbda77c364c37fb37ae
Elron <elron100duck@gmail.com>
2024-08-08 04:57:22 +0300

    Popover on price

--- src/lib/types/item.type.ts
+++ src/lib/types/item.type.ts
@@ -1,14 +1,18 @@
-import { PriceName } from "../enums/price-label.enum";
+// import { PriceName } from "../enums/price-label.enum";
 
-export type Item = {
-  task_id: string;
-  client_thumbprint: string;
-  search_item_timestamp: Date;
-  search_item: string;
-  search_results: number;
-  search_results_captured: number;
-  sum_bundle: number;
-  num_outlier: number;
-  percent_complete: number;
-  n_owled: number;
-} & Record<keyof typeof PriceName, number>
+// export type Item = {
+//   task_id: string;
+//   client_thumbprint: string;
+//   search_item_timestamp: Date;
+//   search_item: string;
+//   search_results: number;
+//   search_results_captured: number;
+//   sum_bundle: number;
+//   num_outlier: number;
+//   percent_complete: number;
+//   n_owled: number;
+// } & Record<keyof typeof PriceName, number>
+
+import type items from '../payload-example-items.json';
+
+export type Item = typeof items[number];
